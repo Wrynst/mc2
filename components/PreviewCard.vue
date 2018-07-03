19976b83727c9b7d0702e5179e633064efeeb63a
Wrynst <n@wrynst.com>
2018-07-02 23:08:10 -0400

    Installed Firebase

---
+++ components/PreviewCard.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3 >
+      <v-card class="preview-card"> 
+      
+        <v-card-media 
+          :src = thumbnail 
+          height="200px"
+        ></v-card-media>
+
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">{{ title }}</h3>
+            <div>{{ teaserTxt }}</div>
+          </div>
+  </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat color="green">Share</v-btn>
+          <v-btn flat color="indigo darken-4" :to="'/posts/' + id">Read</v-btn>
+          <v-btn flat color="orange">Explore</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+   
+</template>
+
+<script>
+  export default {
+    name: 'PreviewCard',
+    props: {
+      id: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      teaserTxt: {
+        type: String,
+        required: true
+      },
+      thumbnail: {
+        type: String,
+        required: true
+      },
+    }
+        
+      
+    
+  }
+</script>
---
+++ components/SpeedDial.vue
@@ -0,0 +1,59 @@
+<template>
+  
+  
+    <v-speed-dial
+      fab
+    >
+      <v-btn
+        slot="activator"
+         v-model="fab"
+        color="blue darken-2"
+        dark
+        
+             small
+            fab
+      >
+        <v-icon>account_circle</v-icon>
+        <v-icon>close</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="green"
+      
+      >
+        <v-icon>edit</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="indigo"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+      <v-btn
+    
+        fab
+        dark
+        small
+        color="red"
+      >
+        <v-icon>delete</v-icon>
+      </v-btn>
+    </v-speed-dial>
+ 
+</template>
+<script>
+  export default {
+    data: () => ({
+      direction: 'left',
+      fab: false,
+      fling: false,
+      hover: true,
+  
+      right: true,
+
+      transition: 'slide-x-reverse-transition'
+    })}
